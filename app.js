b57bdacc5789df8d7124d26e60b4a92c2e5e286d
fana-rkt <fana@n-vibe.fr>
2025-08-25 15:59:32 +0200

    Update develop : missing users

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const PORT = 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const users = [{id: 1, name: 'John Doe'}, {id: 1, name: 'Jane Doe'}];
 app.post('/users' , (req, res) => {
     const newUser = req.body;
     newUser.id = users.length + 1;
     users.push(newUser);
-    resizeTo.status(201).json(newUser);
+    res.status(201).json(newUser);
 });
 
 app.delete('/users/:id', (req, res)=>{
@@ -18,7 +19,7 @@ app.delete('/users/:id', (req, res)=>{
     if(index !== -1){
 
             users.splice(index, 1);
-            req.status(200).send(`User ${userId} deleted`)
+            res.status(200).send(`User ${userId} deleted`)
     } else {
         res.status(404).send('User not found');
     }
