0fc59d0c93c2301bd6016683de0fce18ef9baf57
teetom3 <thomas.zeryouh@gmail.com>
2025-08-25 15:27:02 +0200

    feature delete users

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
-app.post('/users' , (req, res) => {
-    const newUser = req.body;
-    newUser.id = users.length + 1;
-    users.push(newUser);
-    resizeTo.status(201).json(newUser);
-});
+app.delete('/users/:id', (req, res)=>{
+    const userId = parseInt(req.params.id);
+    const index = users.findIndex(user => user.id == userId);
+    if(index !== -1){
 
+            users.splice(index, 1);
+            req.status(200).send(`User ${userId} deleted`)
+    } else {
+        res.status(404).send('User not found');
+    }
+    
+})
