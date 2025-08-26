b0d887a6ece4b33c8003f70e6c657975e3654fd6
fana-rkt <fana@n-vibe.fr>
2025-08-26 10:03:59 +0200

    Add /version route and tests

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
 
 app.get('/status', (req, res) => {
     res.json( { status : 'API is running'} );
-})
+});
+
+app.get('/version', (req, res) => {
+    res.json({ version: '1.0.1' })
+});
 
 module.exports = app;
